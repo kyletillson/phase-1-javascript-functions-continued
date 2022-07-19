fd53a6156e7400858a77d85ee548042a1100fa73
Kyle Tillson <kyletillson004@gmail.com>
2022-07-18 18:34:55 -0600

    commit tree d93d5a73768e8dbe056447ea2cb7c15fcac65fd0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun (event) {
+    if(event === undefined){
+        return "This Saturday, I want to roller-skate!"
+    }   else {
+        return "This Saturday, I want to bathe my dog!"
+    }
+}
+function mondayWork (job) {
+    if(job === undefined){
+        return "This Monday, I will go to the office."
+    }   else {
+        return "This Monday, I will work from home."
+    }
+}
+function wrapAdjective (wrap = '*') {
+    return function(anything){
+        return `You are ${wrap}${anything}${wrap}!`
+    }
+}
